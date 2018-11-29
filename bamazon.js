3c50867cc17057cf0ed230d0439999b6d68ab993
mbag24 <40617051+michaelbaggett@users.noreply.github.com>
2018-11-28 21:24:24 -0600

    made show store func instead of just displaying items at start, then call purchase func within store

--- bamazon.js
+++ bamazon.js
@@ -19,37 +19,41 @@ var connection = mysql.createConnection({
 connection.connect(function (err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId);
+  openStore();
+  connection.end();
+});
+
+
+
+function openStore() {
   connection.query("SELECT * FROM products", function (err, res) {
     if (err) throw err;
 
     for (i = 0; i < res.length; i++) {
       console.log(
-`
+        `
 Product ID: ${res[i].item_id} | Name: ${res[i].product_name} | Price: ${res[i].price} | In Stock: ${res[i].stock_quantity}
 `)
     }
-    //console.log(res);
     userPurchase();
-
-    //user purchase an item function
-    function userPurchase() {
-      inquirer.prompt([{
-          type: "input",
-          message: "Which Product Would you like to purchase?",
-          name: "userChoice"
-        },
-        {
-          type: "input",
-          message: "How many would you like to buy?",
-          name: "userAmt"
-        },
-
-      ]).then(function (inquirerResponse) {
-        const data = inquirerResponse;
-        console.log(data.userChoice, data.userAmt)
-      });
-    }
-    connection.end();
   })
+};
 
-});
+//user purchase an item function
+function userPurchase() {
+  inquirer.prompt([{
+      type: "input",
+      message: "Which Product Would you like to purchase?",
+      name: "userChoice"
+    },
+    {
+      type: "input",
+      message: "How many would you like to buy?",
+      name: "userAmt"
+    },
+
+  ]).then(function (inquirerResponse) {
+    const data = inquirerResponse;
+    console.log(data.userChoice, data.userAmt)
+  });
+}
