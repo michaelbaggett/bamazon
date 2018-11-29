b1d94b5d745c551acdd4f515ccffdac770e13910
mbag24 <40617051+michaelbaggett@users.noreply.github.com>
2018-11-28 20:46:15 -0600

    added purchase function, currently takes in user chocies and logs them to console

--- bamazon.js
+++ bamazon.js
@@ -1,5 +1,6 @@
 var mysql = require("mysql");
-const inquirer = require("inquirer")
+const inquirer = require("inquirer");
+const chalk = require("chalk")
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -15,17 +16,40 @@ var connection = mysql.createConnection({
   database: "bamazon"
 });
 //connect to database & print out product table
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId);
-    connection.query("SELECT * FROM products", function(err, res){
-      if (err) throw err;
-      
-      for (i = 0; i < res.length; i++) {
-        console.log(res[i].item_id, res[i].product_name, res[i].price, res[i].stock_quantity)
-      }
-      //console.log(res);
-      connection.end();
-    }) 
+  connection.query("SELECT * FROM products", function (err, res) {
+    if (err) throw err;
 
-    });
+    for (i = 0; i < res.length; i++) {
+      console.log(
+`
+Product ID: ${res[i].item_id} | Name: ${res[i].product_name} | Price: ${res[i].price} | In Stock: ${res[i].stock_quantity}
+`)
+    }
+    //console.log(res);
+    userPurchase();
+
+    //user purchase an item function
+    function userPurchase() {
+      inquirer.prompt([{
+          type: "input",
+          message: "Which Product Would you like to purchase?",
+          name: "userChoice"
+        },
+        {
+          type: "input",
+          message: "How many would you like to buy?",
+          name: "userAmt"
+        },
+
+      ]).then(function (inquirerResponse) {
+        const data = inquirerResponse;
+        console.log(data.userChoice, data.userAmt)
+      });
+    }
+    connection.end();
+  })
+
+});
